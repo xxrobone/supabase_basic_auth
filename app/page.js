3fe50208e53970ba09ab0a1d895cfc394809c97f
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-05-30 08:35:52 +0200

    i tried to move the node supabase from crud-example and get the json data in a pre tag, works, not done yet

--- app/page.js
+++ app/page.js
@@ -1,27 +1,25 @@
 import Image from 'next/image';
 import styles from './page.module.css';
-/* import { createClient } from '@supabase/supabase-js'; */
+import { createClient } from '@supabase/supabase-js';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies, headers } from 'next/headers';
 
 // if using type script add ! in the env file
 export default async function Home() {
-  console.log('hello');
-  /*  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  ); */
   const supabase = createServerComponentClient({
     headers,
     cookies,
   });
 
-  const { data: news } = await supabase.from('news').select();
+  const { data: characters } = await supabase.from('characters').select();
+
+
   return (
     <main className={styles.main}>
-      <h1>Dance school name</h1>
-      <h2>Senaste:</h2>
-      <pre>{JSON.stringify(news, null, 2)}</pre>
+      <h1>Data from supabase</h1>
+      <h2>data:</h2>
+      <pre>{JSON.stringify(characters, null, 2)}</pre>
+      
     </main>
   );
 }
